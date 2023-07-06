3baa9fc47db699e27e9ab75bcfea08e9bfed156e
boluwatife010 <temitopeboluojo@gmail.com>
2023-07-06 08:29:05 +0100

    Another slight modification

--- simple-array-sum.js
+++ simple-array-sum.js
@@ -1,5 +1,9 @@
 // Algorithm for adding a sum within an array.
-let num = [1, 4, 5, 7, 10];
+function simpleArraySum(num) {
+    num = [1, 4, 5, 7, 10];
 let sum = 0;
 for (let i = 0;  i < num.length; sum += num[i++] );
-console.log(sum);
+return sum;
+}
+let result = simpleArraySum();
+console.log(result);
