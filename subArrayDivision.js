73e2ab5f11e3e10cec207d471034031a82b0e5f6
boluwatife010 <temitopeboluojo@gmail.com>
2023-08-05 18:43:44 +0100

    This is just another problem

---
+++ subArrayDivision.js
@@ -0,0 +1,25 @@
+function birthday(s, d, m) {
+    var sum1 = 0;
+    for (var i = 0; i < s.length - m + 1; i++) {
+        var sum2 = 0;
+        for (var j = 0; j < m; j++) {
+            sum2 += s[i + j];
+        }
+        if (sum2 === d) {
+            sum1++;
+        }
+    }
+    return sum1;
+}
+console.log(birthday([1, 2, 3, 1, 2], 3, 2));
+// We are to print the length of ways an array can be sub-divided.
+// d is the sum of the length of the array.
+// m is the length of the array.
+// s is the array itself.
+//STEPS
+/*
+-> First loop through s
+->state the condition that 2 elements must add up to d
+-> if any of the elements in s sum up to d...
+-> print the number of ways or length in which it can be achieved
+*/ 
